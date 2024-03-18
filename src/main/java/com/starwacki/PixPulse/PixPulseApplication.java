a4841e1d4073df3ba21f367f00c8129b337ff589
Szymon Tarwacki <tarwackiszymon7@gmail.com>
2024-03-18 17:32:40 +0100

    Init commit

---
+++ src/main/java/com/starwacki/PixPulse/PixPulseApplication.java
@@ -0,0 +1,13 @@
+package com.starwacki.PixPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starwacki/PixPulse/PixPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwacki.PixPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
